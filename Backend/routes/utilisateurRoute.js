10ad7e124279793a69c38242d0612369f8394535
Kephira <arielleahlonkoba@gmail.com>
2026-06-16 13:25:45 +0000

    Modification de la page connection 3

--- Backend/routes/utilisateurRoute.js
+++ Backend/routes/utilisateurRoute.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import { registerutilisateur, loginutilisateur ,adminLogin} from '../controller/utilisateurController.js';
 import { getProfilUtilisateur, updateProfilUtilisateur } from '../controller/utilisateurController.js';
-import authUser from '../middleware/authUser.js';
+
 
 const utilisateurRouter = express.Router();
 
 utilisateurRouter.post('/register', registerutilisateur);
 utilisateurRouter.post('/login', loginutilisateur);
 utilisateurRouter.post('/admin',adminLogin)
-utilisateurRouter.get('/profil', authUser, getProfilUtilisateur)
-utilisateurRouter.put('/modifier-profil', authUser, updateProfilUtilisateur)
+utilisateurRouter.get('/profil',  getProfilUtilisateur)
+utilisateurRouter.put('/modifier-profil', updateProfilUtilisateur)
 
 export default utilisateurRouter;
